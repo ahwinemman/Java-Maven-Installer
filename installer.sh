560dd326ca9439cd8db6dad403f7b2d412ed41c3
Oghenerukevwe Ahwin <ahwin@flutterwavego.com>
2020-06-08 08:52:39 +0100

    initial commit

---
+++ installer.sh
@@ -0,0 +1,8 @@
+if  ! mvn clean package;
+then
+  echo 'maven is not installed so use maven wrapper'
+  ./mvnw clean package;
+fi
+docker build -t javaee-injectprops .
+
+docker run -it -p 8080:8080 javaee-injectprops
